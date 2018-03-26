a88c88d732d58ed2ea55e35930e6a441b17c583e
Vinokurov Kirill <vinokurov@elewise.com>
2018-03-26 17:10:54 +0400

    Model update

---
+++ Sixty/Models/Team.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Sixty.Models
+{
+    public class Team : IEntity
+    {
+        #region IEntity interface
+        public virtual Guid Id { get; set; }
+        #endregion
+
+        public virtual string Name { get; set; }
+        public virtual TeamDivision Devision { get; set; }
+
+        public virtual IEnumerable<User> Users { get; set; }
+    }
+
+    public enum TeamDivision
+    {
+        A,
+        B
+    }
+}
---
+++ Sixty/Models/User.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Sixty.Models
+{
+    public class User : IEntity
+    {
+        #region IEntity interface
+        public virtual Guid Id { get; set; }
+        #endregion
+
+        public virtual string Name { get; set; }
+        public virtual string Surname { get; set; }
+        public virtual string Email { get; set; }
+        public virtual string Password { get; set; }
+        public virtual DateTime BirthDate { get; set; }
+
+        public virtual DateTime RegistrationDate { get; set; }
+        public virtual DateTime LastVisitDate { get; set; }
+
+        public virtual Team Team { get; set; }
+    }
+}
