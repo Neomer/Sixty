87c9aec61b7ea9428173e7d5d71115c9049715be
Vinokurov Kirill <vinokurov@elewise.com>
2018-03-28 12:12:20 +0400

    master

---
+++ Sixty/ViewModels/UserProfileViewModel.cs
@@ -0,0 +1,25 @@
+﻿using Sixty.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Sixty.ViewModels
+{
+    public class UserProfileViewModel
+    {
+        public UserProfileViewModel(User user)
+        {
+            Name = user.Name;
+            Surname = user.Surname;
+            BirthDate = user.BirthDate;
+            EMail = user.Email;
+        }
+
+        public string EMail { get; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public DateTime BirthDate { get; set; }
+        public HttpPostedFileBase Photo { get; set; }
+    }
+}
